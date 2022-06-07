ab40453b3af1f3ed09773cd68d25d308b75595c2
Alberto <alberto_januario@hotmail.com>
2022-06-07 09:02:34 -0300

    prova semestral de java

---
+++ gs-ead/gco-ead/src/main/java/exception/CommitException.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class CommitException extends Exception{
+	
+	public CommitException() {
+		super("Erro ao realizar o commit");
+	}
+	
+	public CommitException(String msg) {
+		super(msg);
+	}
+	
+}
---
+++ gs-ead/gco-ead/src/main/java/exception/IdNotFindException.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class IdNotFindException extends Exception{
+	
+	public IdNotFindException() {
+		super("Id não encontrado");
+	}
+	
+	public IdNotFindException(String msg) {
+		super(msg);
+	}
+}
